ced9f1c4de799d4c7a86c4c60f114f38169e5cb6
nopalpite <nopalpite@gmail.com>
2023-07-29 20:09:58 +0200

    migration and templates and url fix

--- lettings/views.py
+++ lettings/views.py
@@ -4,7 +4,7 @@ from .models import Letting
 def index(request):
     lettings_list = Letting.objects.all()
     context = {'lettings_list': lettings_list}
-    return render(request, 'index.html', context)
+    return render(request, 'lettings/index.html', context)
 
 
 def letting(request, letting_id):
@@ -13,4 +13,4 @@ def letting(request, letting_id):
         'title': letting.title,
         'address': letting.address,
     }
-    return render(request, 'letting.html', context)
+    return render(request, 'lettings/letting.html', context)
--- profiles/views.py
+++ profiles/views.py
@@ -4,9 +4,9 @@ from .models import Profile
 def index(request):
     profiles_list = Profile.objects.all()
     context = {'profiles_list': profiles_list}
-    return render(request, 'index.html', context)
+    return render(request, 'profiles/index.html', context)
 
 def profile(request, username):
     profile = Profile.objects.get(user__username=username)
     context = {'profile': profile}
-    return render(request, 'profile.html', context)
+    return render(request, 'profiles/profile.html', context)
